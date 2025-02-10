359659381896c4cd8b017e6ad2e30dfd601f8b8b
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2025-02-10 20:24:57 +0200

    fixed RandomInstance

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,20 +4,20 @@ import kotlin.random.Random
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.javaType
-import kotlin.reflect.jvm.jvmName
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun <T: Any>createRandomInstance(kClass: KClass<T>): T? {
-    val constructor = kClass.primaryConstructor ?: return null
+inline fun <reified T> createRandomInstance(): T? {
+    if (T::class.java.isPrimitive)
+        return null
+    val constructor = T::class.constructors.first()
     val args = mutableListOf<Any?>()
     for (i: KParameter in constructor.parameters) {
-        when (i.type.toString()) {
-            Double::class.qualifiedName -> args.add(Random.nextDouble())
-            Int::class.qualifiedName -> args.add(Random.nextInt())
-            Boolean::class.qualifiedName -> args.add(Random.nextBoolean())
-            String::class.qualifiedName -> args.add(String(CharArray(7) { ('a'..'z').random() }))
+        when (i.type.classifier) {
+            Double::class -> args.add(Random.nextDouble())
+            Int::class -> args.add(Random.nextInt())
+            Boolean::class -> args.add(Random.nextBoolean())
+            String::class -> args.add(String(CharArray(7) { ('a'..'z').random() }))
         }
     }
     return constructor.call(*args.toTypedArray())
